2e92bc966604b7a6c88e4f3a79aecdedad1f5c69
Vandana Singh <108992234+Vandy0814@users.noreply.github.com>
2022-07-16 16:52:29 +0530

    Committing Add/Delete/Search Screen files

---
+++ simplilearn/phase/one/project/Constants.java
@@ -0,0 +1,14 @@
+package simplilearn.phase.one.project;
+
+public class Constants {
+	
+	//Home Directory path (Parent Directory)
+	public static final String path = "C:\\Vandana\\Parent"; 
+	
+	//Main Options from Welcome Screen
+	public static final String[] mainOptions = {"1 LIST ALL FILES","2 ADD/DELETE/SEARCH FILES","3 CLOSE APPLICATION"};
+	
+	//Sub Options for Main Option = 2
+	public static final String[] subOptions = {"1 ADD FILE/FILES","2 DELETE FILE/FILES","3 SEARCH FILE/FILES", "4 GO BACK TO MAIN SCREEN"};
+
+}
